39788f776289c0f55f325d3fc086aa82339a8106
Piotr <piotr.bania@europe.com>
2023-02-13 22:32:39 +0000

    init

--- components/homepage/Experience.js
+++ components/homepage/Experience.js
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import { AnimatePresence, motion as m } from 'framer-motion'
 import Planet_1 from './Planet_1'
 import { EffectComposer, Noise, Scanline, Bloom, Vignette, Glitch } from '@react-three/postprocessing'
-import { Environment } from '@react-three/drei'
+import { Environment, Lightformer } from '@react-three/drei'
 
 const Experience = () => {
     return (
@@ -13,24 +13,23 @@ const Experience = () => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, transition: { delay: 10, duration: 2 } }}
                 >
-                <Canvas camera={{
-                    position: [-1, 0, 6],
+                <Canvas shadows camera={{
+                    position: [-1, 0.5, 4],
                     rotation: [0, 0, 0],
                     fov: 25 }}>
                     <Suspense fallback={null}>
                         <Planet_1 />
                     </Suspense>
-
-                    <Environment files={'./environment/gamrig_4k.hdr'} />
+                    <Environment files={'./environment/kloppenheim_02_puresky_4k.hdr'} background blur={0.025} />
                     
                     <EffectComposer>
-                        <Scanline density={1} opacity={0.35}/>
-                        <Noise opacity={0.35} />
-                        <Vignette eskil={false} offset={0.1} darkness={1.5} />
+                        <Scanline density={1.5} opacity={0.15}/>
+                        <Noise opacity={0.25} />
+                        <Vignette eskil={false} offset={0.55} darkness={0.55} />
                         <Glitch
                         delay={[0.5, 5]}
-                        duration={[0.05, 0.5]}
-                        strength={[0.1, 0.5]}
+                        duration={[0.01, 0.5]}
+                        strength={[0.01, 0.5]}
                         ratio={0.95}
                         />
                     </EffectComposer>
--- components/homepage/Planet_1.js
+++ components/homepage/Planet_1.js
@@ -2,25 +2,25 @@ import React, { useRef } from 'react'
 import { useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-
 const Planet_1 = () => {
 
     const planetRef = useRef()
 
     useFrame((state, delta) => {
-        planetRef.current.rotation.y -= 0.002,
-        planetRef.current.rotation.x -= 0.001
+        planetRef.current.rotation.x += 0.0001,
+        planetRef.current.rotation.y -= 0.0002,
+        planetRef.current.rotation.z -= 0.0003
     })
 
-    const planet = useLoader(GLTFLoader, '/models/homepage/hero_planet.gltf')
+    const planet = useLoader(GLTFLoader, '/models/test/planet.gltf')
 
     return (
         <>
-            <ambientLight intensity={0.25} color={'#61F570'} />
+            <ambientLight intensity={0.5} color={'#61F570'} />
             <pointLight castShadow position={[-2, -1, 2]} color={'#7161F5'} intensity={0.75} />
-            <pointLight castShadow position={[5, 3, -2]} color={'#A362F5'} intensity={0.75} />
+            <pointLight castShadow position={[2, 3, -2]} color={'#A362F5'} intensity={0.75} />
 
-            <mesh ref={planetRef}>
+            <mesh castShadow receiveShadow ref={planetRef} scale={[0.1, 0.1, 0.1]} rotation={[0, 0, 0]}>
                 <primitive object={planet.scene}/>
             </mesh>
         </>
